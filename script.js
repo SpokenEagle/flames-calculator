37bbc78e6533a969b7e96a3661c7d6529b128784
zaarzet <zaarzet@gmail.com>
2023-07-23 21:03:54 +0530

    Fixed calculate button not working in the first run of script

--- script.js
+++ script.js
@@ -8,7 +8,35 @@ function checkInput() {
         alert("Please enter different names");
     }
     else{
-        findFlames();
+        // var n = document.getElementById("person1").value;
+        n=n.toLowerCase().replace(" ", "");
+        // var m=document.getElementById("person2").value;
+        m=m.toLowerCase().replace(" ", "");
+
+
+
+        console.log(n);
+        console.log(m);
+
+        var game, unique;
+
+        if (n.length < m.length) {
+            unique = get_unique(n, m);
+        }
+        else if(n.length > m.length){
+            unique = get_unique(m, n);
+        }
+        else{
+            unique = get_unique(m, n);
+        }
+
+        console.log("unique : ", unique);
+        if(unique===0){
+            alert("No unique letters in the given names. Try entering different names");
+        }
+        else{
+            findFlames(n,m,unique);
+        }
     }
 }
 // -----------------------------------------------------------------
@@ -17,62 +45,32 @@ function checkInput() {
 function resetStuff() {
     document.getElementById("person1").value = "";
     document.getElementById("person2").value = "";
-    document.getElementById("result").innerHTML = "";
+    document.getElementById("result").innerHTML = "&nbsp;";
 }
 // -----------------------------------------------------------------
-function findFlames(){
+function get_unique(a, b) {
+    var count;
+    count = 0;
 
-    function get_unique(a, b) {
-        var count;
-        count = 0;
-
-        for (var i = 0, _pj_a = a.length; i < _pj_a; i += 1) {
-            for (var j = 0, _pj_b = b.length; j < _pj_b; j += 1) {
-                if (a[i] === b[j]) {
-                    b = b.slice(0, j) + b.slice(j + 1);
-                    count += 1;
-                    break;
-                }
+    for (var i = 0, _pj_a = a.length; i < _pj_a; i += 1) {
+        for (var j = 0, _pj_b = b.length; j < _pj_b; j += 1) {
+            if (a[i] === b[j]) {
+                b = b.slice(0, j) + b.slice(j + 1);
+                count += 1;
+                break;
             }
         }
-
-        console.log("len a [Without Removing duplicates]", a.length);
-        console.log("count", count);
-        console.log("leb b [After Removing duplicates]", b.length);
-        return a.length - count + b.length;
     }
 
-
-    var n = document.getElementById("person1").value;
-    n=n.toLowerCase().replace(" ", "");
-    var m=document.getElementById("person2").value;
-    m=m.toLowerCase().replace(" ", "");
-
-
+    console.log("len a [Without Removing duplicates]", a.length);
+    console.log("count", count);
+    console.log("leb b [After Removing duplicates]", b.length);
+    return a.length - count + b.length;
+}
+// ---------------------------------------------------------------
+function findFlames(n,m,unique){
     var finalText="";
-    if (n==="hemanth" || m==="hemanth"){
-        finalText+="Coding Chesko vro, ilanti websites lo time waste cheyyaku\n\n<br><br> Ni result : ";
-        console.log("$$$$$$$$$$$$$$$$$$$$$$");
-    }
-
-    console.log(n);
-    console.log(m);
-
-    var game, unique;
-
-    if (n.length < m.length) {
-        unique = get_unique(n, m);
-    } else {
-        if (n.length > m.length) {
-            unique = get_unique(m, n);
-        } else {
-            unique = get_unique(m, n);
-        }
-    }
-
     game = "FLAMES";
-    console.log("unique : ", unique);
-
     function findLEFT(unique, s, position) {
         var i, index;
         i = 0;
@@ -95,7 +93,6 @@ function findFlames(){
     var game, position;
     position = 0;
     console.log(`[${game}]`);
-
     while (game.length !== 1) {
         [game, position] = findLEFT(unique, game, position);
         console.log(`[${game}]`, position);
